ab4c7d9eb838f34d48f62cf4df0d5f360f0a708b
niranaik1606 <niranaik1606@gmail.com>
2026-03-28 11:01:23 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("nira.manjunath.naik");
+	System.out.println("std. X");
+	System.out.println("Roll no. 5");
+	System.out.println("St. Francis School");
+	}
+
+}
