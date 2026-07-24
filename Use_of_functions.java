0724c337c2493e5997387980df6e4a08c30b9166
niranaik1606 <niranaik1606@gmail.com>
2026-07-24 13:43:16 +0530

    Add files via upload

---
+++ Use_of_functions.java
@@ -0,0 +1,18 @@
+package java_world;
+
+public class Use_of_functions {
+
+	public static void main(String[] args) {
+	
+		
+	int sum=addition(2,3,4);
+	System.out.println(sum);
+
+	}
+	
+	public static int addition(int a, int b, int c) {
+		return a+b+c; 
+	}
+
+}
+    
