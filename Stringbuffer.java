fb615d3e857cea8e31e3082983a3c89e72698b31
niranaik1606 <niranaik1606@gmail.com>
2026-08-04 13:56:01 +0530

    Add files via upload

---
+++ Stringbuffer.java
@@ -0,0 +1,14 @@
+package java_world;
+
+public class Stringbuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String st1="Hello";
+	StringBuffer Sbl= new StringBuffer (st1);
+	Sbl.reverse();
+	System.out.println(Sbl);
+	}
+
+}
+ 
